d4ccc71b9deb13d7e4e699dd0d2d3200b0bab824
Victor Raony <victor_raony12@hotmail.com>
2022-02-10 12:55:44 -0400

    Views

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
