86ea1bbd41a755de147046f8a8612aa97e1dcc5d
Victor Raony <victor_raony12@hotmail.com>
2022-02-10 17:06:27 -0400

    Removido view contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Victor Raony',
+    })
